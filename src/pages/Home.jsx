10a20ad77b3fd178f4d55d37cf02b9b7f6a45234
gndsulistiawan <gndsulistiawan@gmail.com>
2023-11-13 23:09:41 +0700

    feat: bergabung section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
@@ -5,6 +6,14 @@ const Home = () => {
   return (
     <>
       <Header />
+      <section className="w-full h-72 bg-primary/10 flex flex-col justify-center items-center gap-11">
+        <div className="text-3xl font-semibold text-black">
+          Tunggu Apa Lagi? GabungBersama Kami!
+        </div>
+        <Link className="btn btn-primary text-white capitalize font-semibold text-lg">
+          Daftar Sekarang
+        </Link>
+      </section>
       <Footer />
     </>
   );
