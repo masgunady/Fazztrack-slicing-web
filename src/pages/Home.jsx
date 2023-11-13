4c528454dc8ff41e5b6747cc5a53d3f56180f484
gndsulistiawan <gndsulistiawan@gmail.com>
2023-11-14 00:12:24 +0700

    feat: card alumny story section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,93 @@ const Home = () => {
       <Header />
       <section className="w-full flex flex-col justify-center items-center gap-11 py-11">
         <div className="w-full max-w-[1080px] text-black flex flex-col gap-3">
+          <div className="w-full flex justify-between items-center my-9">
+            <div className="flex flex-col gap-2">
+              <div className="text-black text-2xl font-bold">
+                Cerita Alumni Bootcamp
+              </div>
+              <div className="text-slate-500 text-xl font-semibold">
+                Cerita perjalanan heroik alumni bootcamp
+              </div>
+            </div>
+            <div>
+              <Link
+                to={""}
+                className="btn btn-neutral rounded bg-slate-50 border-slate-200 hover:bg-slate-200 hover:border-slate-200 capitalize text-black font-semibold text-lg"
+              >
+                Lihat cerita lainnya
+              </Link>
+            </div>
+          </div>
+          <div className="w-full flex justify-between items-center gap-5">
+            <div className="card w-96 bg-base-100 shadow-xl">
+              <figure>
+                <img
+                  src="https://www.fazztrack.com/_ipx/f_webp&q_100&fit_cover&s_586x328/img/alumni-story/alumni-story-1.png"
+                  alt="Shoes"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">
+                  Lulusan Pesantren juga Bisa jadi DevOps Engineer
+                </h2>
+                <div>
+                  Menjadi seorang santri sekaligus pelajar, bukanlah hal yang
+                  mudah. Menjalani dua kegiatan secara bersamaan membuat saya
+                  merasa kurang waktu belajar pemrograman disekolah. Banyak
+                  tantangan yang harus dihadapi. Mulai dari bagaimana membagi
+                  waktu antara mengaji, belajar akademik dan upgrade skills
+                  programming.
+                </div>
+              </div>
+            </div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+              <figure>
+                <img
+                  src="https://www.fazztrack.com/_ipx/f_webp&q_100&fit_cover&s_586x328/img/alumni-story/alumni-story-1.png"
+                  alt="Shoes"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">
+                  Lulusan Pesantren juga Bisa jadi DevOps Engineer
+                </h2>
+                <div>
+                  Menjadi seorang santri sekaligus pelajar, bukanlah hal yang
+                  mudah. Menjalani dua kegiatan secara bersamaan membuat saya
+                  merasa kurang waktu belajar pemrograman disekolah. Banyak
+                  tantangan yang harus dihadapi. Mulai dari bagaimana membagi
+                  waktu antara mengaji, belajar akademik dan upgrade skills
+                  programming.
+                </div>
+              </div>
+            </div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+              <figure>
+                <img
+                  src="https://www.fazztrack.com/_ipx/f_webp&q_100&fit_cover&s_586x328/img/alumni-story/alumni-story-1.png"
+                  alt="Shoes"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">
+                  Lulusan Pesantren juga Bisa jadi DevOps Engineer
+                </h2>
+                <div>
+                  Menjadi seorang santri sekaligus pelajar, bukanlah hal yang
+                  mudah. Menjalani dua kegiatan secara bersamaan membuat saya
+                  merasa kurang waktu belajar pemrograman disekolah. Banyak
+                  tantangan yang harus dihadapi. Mulai dari bagaimana membagi
+                  waktu antara mengaji, belajar akademik dan upgrade skills
+                  programming.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="w-full flex flex-col justify-center items-center gap-11 py-11">
+        <div className="w-full max-w-[1080px] text-black flex flex-col gap-3">
           <div className="w-full flex flex-col justify-center items-center gap-5 py-3">
             <div className="text-black text-2xl capitalize font-bold">
               Pertanyaan yang sering ditanyakan
@@ -85,7 +172,7 @@ const Home = () => {
           <div className="w-full flex justify-center items-center py-7">
             <Link
               to={""}
-              className="btn btn-neutral bg-slate-100 border-slate-300 hover:bg-slate-300 hover:boreder-slate-300 capitalize text-black font-semibold text-lg"
+              className="btn btn-neutral rounded bg-slate-50 border-slate-200 hover:bg-slate-200 hover:border-slate-300 capitalize text-black font-semibold text-lg"
             >
               Pelajari Selengkapnya
             </Link>
